f24d10a07f8f0823a1fbbace832c5eee88dd0564
jcarlossc <josecscosta@fac.pe.senac.br>
2025-09-17 16:43:16 -0300

    Arquivo tsc.

--- open-files.R
+++ open-files.R
@@ -17,9 +17,40 @@
 # dec="," - O caractere usado para os pontos decimais. O padrão é o ponto (.). 
 # Entre outros...
 
-arquivo.csv <-read.csv("files/vendas.csv")
+# Para mais informações sobre a função read.csv(), retire o cerquilha e
+# execute o comando:
+# ?read.csv()
+
+arquivo_csv <-read.csv("files/vendas.csv")
 
 # Apresenta os primeiros registros do arquivo.
-head(arquivo.csv)
+head(arquivo_csv)
 
 #-------------------------------------------------
+
+# ARQUIVO TSV.
+
+# Um arquivo TSV (Tab-Separated Values) é um formato de 
+# texto simples que organiza dados tabulares, onde cada 
+# linha de dados é separada por um caractere de tabulação. 
+# É uma alternativa ao formato CSV e é amplamente usado para 
+# importar e exportar dados entre diferentes aplicações, 
+#como planilhas eletrônicas e bancos de dados.
+
+# A função read.csv() pode precisar de parâmetros, por exemplos:
+# sep="" - arquivo csv separado por ponto e vírgula.
+# header=TRUE ou header=FALSE - os nomes das colunas ou  não, respectivamente.
+# dec="," - O caractere usado para os pontos decimais. O padrão é o ponto (.). 
+# Entre outros...
+
+# Para mais informações sobre a função read.delim(), retire o cerquilha e
+# execute o comando:
+# ?read.delim()
+
+arquivo_tsv <- read.delim("files/vendas.tsv")
+
+# Apresenta os primeiros registros do arquivo.
+head(arquivo_tsv)
+
+#-------------------------------------------------
+
